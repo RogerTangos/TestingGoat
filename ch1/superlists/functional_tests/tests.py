55f9071e3c4e77d181d00c64cd9a2c49a312d5a9
Albert Carter <albert.r.carter@gmail.com>
2015-09-25 14:48:30 -0400

    p080 - use LiveServerTestCase class'

--- ch1/superlists/functional_tests/tests.py
+++ ch1/superlists/functional_tests/tests.py
@@ -1,12 +1,14 @@
+from django.test import LiveServerTestCase
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitor(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
     def tearDown(self):
         self.browser.quit()
@@ -60,6 +62,3 @@ class NewVisitorTest(unittest.TestCase):
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
