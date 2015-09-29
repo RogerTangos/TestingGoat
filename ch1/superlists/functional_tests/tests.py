02a9e7a684e4d9124bade1691df1214eaa2e22a8
Albert Carter <albert.r.carter@gmail.com>
2015-09-29 17:03:42 -0400

    p096 - pep8 cleanup

--- ch1/superlists/functional_tests/tests.py
+++ ch1/superlists/functional_tests/tests.py
@@ -4,7 +4,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 
+
 class NewVisitor(LiveServerTestCase):
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -33,14 +35,14 @@ class NewVisitor(LiveServerTestCase):
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
-            )
+        )
 
         # She types "Buy peacock feathers" into a  text box (Edith's hobby
         # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, she is taken to a new URL,
-        # and now the page lists "1: Buy peacock feathers" 
+        # and now the page lists "1: Buy peacock feathers"
         # as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
         edith_list_url = self.browser.current_url
@@ -48,19 +50,21 @@ class NewVisitor(LiveServerTestCase):
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly')
 
         # Now a new user, Francis, comes along to the site.
 
-        ## We use a new browser session to make sure that no information
-        ## of Edith's is coming from cookies, etc
+        # We use a new browser session to make sure that no information
+        # of Edith's is coming from cookies, etc
         self.browser.quit()
         self.browser = webdriver.Firefox()
 
--- ch1/superlists/lists/views.py
+++ ch1/superlists/lists/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import redirect, render
 from lists.models import Item
 
-# Create your views here.
+
 def home_page(request):
-	return render(request, 'home.html')
+    return render(request, 'home.html')
+
 
 def view_list(request):
-	items = Item.objects.all()
-	return render(request, 'list.html', {'items':items})	
+    items = Item.objects.all()
+
+    return render(request, 'list.html', {'items': items})
+
 
 def new_list(request):
     Item.objects.create(text=request.POST['item_text'])
