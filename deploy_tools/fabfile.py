8b2f4a36af97eacbee9597de61065b26e4f5b1ae
Albert Carter <albert.r.carter@gmail.com>
2015-10-06 15:55:17 -0400

    p166 - Finish ch 9, skipping sed section

---
+++ deploy_tools/fabfile.py
@@ -0,0 +1,76 @@
+from fabric.contrib.files import append, exists, sed
+from fabric.api import env, run, local
+import random
+
+REPO_URL = 'https://github.com/RogerTangos/TestingGoat.git'
+
+
+def deploy():
+    site_folder = '/home/%s/sites/%s' % (env.user, env.host)
+    source_folder = site_folder + '/source'
+    _create_directory_structure_if_necessary(site_folder)
+    _get_latest_source(source_folder)
+    _update_settings(source_folder, env.host)
+    _update_virtualenv(source_folder)
+    _update_static_files(source_folder)
+    _update_database(source_folder)
+
+
+def _create_directory_structure_if_necessary(site_folder):
+    for subfolder in ('database', 'static', 'virtualenv', 'source'):
+        run('mkdir -p %s/%s' % (site_folder, subfolder))
+
+
+def _get_latest_source(source_folder):
+    # checks to see whether .git dir already exists
+    if exists(source_folder + '/.git'):
+        # runs a git fetch
+        run('cd %s && git fetch' % (source_folder,))
+    else:
+        # otherwise, clones from the repo
+        run('git clone %s %s' % (REPO_URL, source_folder))
+    current_commit = local("git log -n 1 --format=%H", capture=True)
+    # checks out the most current commit, and resets all local changes
+    run('cd %s && git reset --hard %s' % (source_folder, current_commit))
+
+
+def _update_settings(source_folder, site_name):
+    settings_path = source_folder + '/superlists/settings.py'
+    # sed = str substitution
+    sed(settings_path, "DEBUG = True", "DEBUG = False")
+    sed(settings_path,
+        'ALLOWED_HOSTS =.+$',
+        'ALLOWED_HOSTS = ["%s"]' % (site_name,)
+        )
+    secret_key_file = source_folder + '/superlists/secret_key.py'
+    if not exists(secret_key_file):
+        # make a unique secret key in deployment
+        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+        key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
+        append(secret_key_file, "SECRET_KEY = '%s'" % (key,))
+    # make sure that it adds this to the end of the file
+    # import is relative, to make sure that it's being imported from the local
+    # module, instead of somewhere else on sys.path
+    append(settings_path, '\nfrom .secret_key import SECRET_KEY')
+
+
+def _update_virtualenv(source_folder):
+    virtualenv_folder = source_folder + '/../virtualenv'
+    # look inside of virtualenvs for pip and install if it isn't there
+    if not exists(virtualenv_folder + '/bin/pip'):
+        run('virtualenv --python=python3 %s' % (virtualenv_folder,))
+    run('%s/bin/pip install -r %s/requirements.txt' % (
+            virtualenv_folder, source_folder
+    ))
+
+
+def _update_static_files(source_folder):
+    run('cd %s && ../virtualenv/bin/python3 manage.py collectstatic --noinput'
+        % (source_folder)
+        )
+
+
+def _update_database(source_folder):
+    run('cd %s && ../virtualenv/bin/python3 manage.py migrate --noinput' % (
+        source_folder,
+    ))
