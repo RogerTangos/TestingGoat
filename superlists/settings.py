5709290f47b41175a2b6c961bfb4d1997400a6e3
Albert Carter <albert.r.carter@gmail.com>
2015-10-06 17:21:07 -0400

    p175 - move unit tests to a dedicated folder, re-activate debug mode

--- superlists/settings.py
+++ superlists/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 SECRET_KEY = 'm_*#^bd@^6=#_1p#72yccx%%!_)@$r72uc9v0#(3j=k*t_6&id'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_DEBUG = DEBUG
 
